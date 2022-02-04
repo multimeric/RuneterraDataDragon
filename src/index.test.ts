d806bfd92a4e469cf0eb16561d70dcb6b40448e3
Michael Milton <michael.r.milton@gmail.com>
2022-02-04 18:51:51 +1100

    Initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,71 @@
+import {LorDataDragon} from "./index";
+
+describe("getSetCards", () => {
+    it("Should return an array of cards", async () => {
+        const dd = new LorDataDragon({
+            cacheDir: "./testCache"
+        });
+        const cards = await dd.getSetCards(2);
+        expect(cards.length).toBeGreaterThan(100);
+        expect(cards[0]).toEqual({
+                "associatedCards": [],
+                "associatedCardRefs": [],
+                "assets": [
+                    {
+                        "gameAbsolutePath": "http://dd.b.pvp.net/3_2_0/set2/en_us/img/cards/02PZ001.png",
+                        "fullAbsolutePath": "http://dd.b.pvp.net/3_2_0/set2/en_us/img/cards/02PZ001-full.png"
+                    }
+                ],
+                "regions": [
+                    "Piltover & Zaun"
+                ],
+                "regionRefs": [
+                    "PiltoverZaun"
+                ],
+                "attack": 1,
+                "cost": 5,
+                "health": 5,
+                "description": "When I'm summoned, draw 1.\r\nThen, if you've played at least 10 other cards with different names, grant me +4|+0. <style=Variable></style>",
+                "descriptionRaw": "When I'm summoned, draw 1.\r\nThen, if you've played at least 10 other cards with different names, grant me +4|+0. ",
+                "levelupDescription": "",
+                "levelupDescriptionRaw": "",
+                "flavorText": "Nothing could stop his escape from those simpering humans. Not even a little water...",
+                "artistName": "SIXMOREVODKA",
+                "name": "Subpurrsible",
+                "cardCode": "02PZ001",
+                "keywords": [
+                    "Elusive"
+                ],
+                "keywordRefs": [
+                    "Elusive"
+                ],
+                "spellSpeed": "",
+                "spellSpeedRef": "",
+                "rarity": "EPIC",
+                "rarityRef": "Epic",
+                "subtypes": [],
+                "supertype": "",
+                "type": "Unit",
+                "collectible": true,
+                "set": "Set2"
+            },
+        );
+    }, 100_000)
+})
+
+describe("getGlobalData", () => {
+    it("Should return global data", async () => {
+        const dd = new LorDataDragon({
+            cacheDir: "./testCache"
+        });
+        const global = await dd.getGlobalData();
+        expect(global.spellSpeeds.length).toEqual(3);
+        expect(global.vocabTerms[0]).toEqual(
+            {
+                "description": "When you summon this, it gets its allegiance bonus if the top card of your deck matches its region.",
+                "name": "Allegiance",
+                "nameRef": "Allegiance"
+            },
+        )
+    }, 100_000)
+})
---
+++ src/types.ts
@@ -0,0 +1,92 @@
+export enum Locale {
+    German = "de_de",
+    English = "en_us",
+    SpanishSpain = "es_es",
+    SpanishMexico = "es_mx",
+    French = "fr_fr",
+    Italian = "it_it",
+    Japanese = "ja_jp",
+    Korean = "ko_kr",
+    Polish = "pl_pl",
+    Portuguese = "pt_br",
+    Thai = "th_th",
+    Turkish = "tr_tr",
+    Russian = "ru_ru",
+    Chinese = "zh_tw"
+}
+
+export interface Asset {
+    gameAbsolutePath: string,
+    fullAbsolutePath: string
+}
+
+export enum Rarity {
+    Common = "common",
+    Rare = "rare",
+    Epic = "epic",
+    Champion = "champion"
+}
+
+export enum SpellSpeed {
+    Burst = "burst",
+    Fast = "fast",
+    Slow = "slow",
+    Focus = "focus"
+}
+
+export interface Card {
+    associatedCards: any[],
+    associatedCardRefs: string[],
+    assets: Asset[],
+    regions: string[],
+    regionRefs: string[],
+    attack: number,
+    cost: number,
+    health: number,
+    description: string,
+    descriptionRaw: string,
+    levelUpDescription: string,
+    levelUpDescriptionRaw: string,
+    flavorText: string,
+    artistName: string,
+    name: string,
+    cardCode: string
+    keywords: string[],
+    keywordRefs: string[],
+    spellSpeed: SpellSpeed,
+    spellSpeedRef: SpellSpeed,
+    rarity: Rarity,
+    rarityRef: Rarity,
+    subtypes: string[],
+    supertype: string,
+    type: string,
+    collectible: boolean,
+    set: string
+}
+
+export interface GlobalEntry {
+    name: string,
+    nameRef: string
+}
+
+export interface Keyword extends GlobalEntry {
+    description: string,
+}
+
+export interface Region extends GlobalEntry {
+    abbreviation: string,
+    iconAbsolutePath: string,
+}
+
+export interface SetData extends GlobalEntry {
+    iconAbsolutePath: string
+}
+
+export interface GlobalData {
+    vocabTerms: Keyword[],
+    keywords: Keyword[],
+    regions: Region[],
+    spellSpeeds: GlobalEntry[],
+    rarities: GlobalEntry[],
+    sets: SetData
+}
